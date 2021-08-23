8e8180cdc86a066de00b1f58f8f32dd2ad58a618
tareq179 <abdulkarimctg373@gmail.com>
2021-08-23 23:00:28 +0600

    add api file

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+
+
+dotenv.config();
+const app = express();
+const port = 8800;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfully"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+
+app. use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movie", movieRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
