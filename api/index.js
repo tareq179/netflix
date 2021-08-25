dc019fc974e8799fba9a6522dad37d67900a9b20
tareq179 <abdulkarimctg373@gmail.com>
2021-08-25 10:36:33 +0600

    add list api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 
 dotenv.config();
@@ -26,6 +27,7 @@ app.use(express.json());
 app. use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movie", movieRoute);
+app.use("/api/list", listRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -48,9 +48,7 @@ router.put("/:id", verify, async (req, res) => {
 router.delete("/:id", verify, async (req, res) => {
   if (req.user.isAdmin) {
     try {
-      await Movie.findByIdAndDelete(
-        req.params.id
-      );
+      await Movie.findByIdAndDelete(req.params.id);
       res.status(200).json("The movie has been deleted.....!");
     } catch (err) {
       res.status(500).json(err);
@@ -61,37 +59,50 @@ router.delete("/:id", verify, async (req, res) => {
 });
 //GET
 
-router.get("/:id", verify, async (req, res) => {
-    try {
-      const movie= await Movie.findById(
-        req.params.id
-      );
-      res.status(200).json(movie);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+router.get("/find/:id", verify, async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 //GET RANDOM
 
 router.get("/random", verify, async (req, res) => {
-    const type = req.query.type;
-    let movie;
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === "series") {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: true } },
+        { $sample: { size: 1 } },
+      ]);
+    } else {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: false } },
+        { $sample: { size: 1 } },
+      ]);
+    }
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get ALL
+
+router.get("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
     try {
-      if(type === "series"){
-          movie = await Movie.aggregate([
-              {$match:{isSeries:true}},
-              {$sample:{size:1}}
-          ])
-      } else{
-        movie = await Movie.aggregate([
-            {$match:{isSeries:false}},
-            {$sample:{size:1}}
-        ])
-      }
-      res.status(200).json(movie);
+      const movies = await Movie.find();
+      res.status(200).json(movies.reverse());
     } catch (err) {
       res.status(500).json(err);
     }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
 });
 
 module.exports = router;
